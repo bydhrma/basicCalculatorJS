b504c1dc2d517bbcfa3df4ef259f467833a90d89
bayu <bayudharmap@gmail.com>
2022-06-20 21:08:22 +0800

    Added delete,display and operation function

--- script.js
+++ script.js
@@ -9,29 +9,72 @@ class Calculator {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
+        
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-        this.currentOperand = number
+        if (number === '.' && this.currentOperand.includes('.')) return 
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-
+        if(this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+     this.operation = operation
+     this.previousOperand = this.currentOperand
+     this.currentOperand = ''
     }
 
     compute() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return 
+        switch(this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            case 'x':
+                computation = prev * current
+                break    
+            default:
+                return    
+        }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+        this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+
+        }
     }
 
+    getDisplay() {
+        const floatNumber = parseFloat(number)
+        if (isNaN(floatNumber)) return ''
+        return floatNumber.toLocaleString('en')
+        }
 }
+
+
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons =document.querySelectorAll('[data-operation]');
@@ -52,3 +95,26 @@ numberButtons.forEach(button => {
     })
 })
 
+operationButtons.forEach(button => {
+    button.addEventListener ('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener ('click' , button => {
+    calculator.compute()
+    calculator.updateDisplay()
+    
+} )
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click' , button => {
+    calculator.delete()
+    calculator.updateDisplay()
+
+})
